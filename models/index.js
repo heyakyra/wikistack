3bf84f0d4d5ffaa94f2287557dcef20951c2cbdc
Molly Hanes <mollyfhanes@gmail.com>
2018-03-09 11:18:24 -0600

    molly updates 12:18 PM

--- models/index.js
+++ models/index.js
@@ -1,6 +1,24 @@
 const Sequelize = require("sequelize");
 const db = new Sequelize("postgres://localhost:5432/wikistack");
 
+const Page = db.define('page', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  slug: {
+    type: Sequelize.STRING
+  },
+  content: {
+    type: Sequelize.TEXT
+  },
+  status: {
+    type: Sequelize.BOOLEAN
+  }
+})
+
+
+
 module.exports = {
   db
 };
